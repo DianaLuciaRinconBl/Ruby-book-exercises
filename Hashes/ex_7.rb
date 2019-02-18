da01edf318246192e93cab2da21806c85e644e6a
DianaLuciaRinconBl <45254887+DianaLuciaRinconBl@users.noreply.github.com>
2019-02-18 21:10:19 +1100

    Exercises of the Hashes chapter

---
+++ Hashes/ex_7.rb
@@ -0,0 +1,7 @@
+#Given the following code...
+
+x = "hi there"
+my_hash = {x: "some value"}#the key of this hash is a symbol
+my_hash2 = {x => "some value"}#the key of this hash is a string
+
+#What's the difference between the two hashes that were created?
